fe508e8e0348d398b98ae066dd9c9edadffc04c7
RojasD13 <94007095+RojasD13@users.noreply.github.com>
2025-10-13 16:28:52 -0500

    fix: se añade un ejemplo en properties para que no busque .env.properties

--- src/test/java/com/edu/uptc/loginService/LoginServiceApplicationTests.java
+++ src/test/java/com/edu/uptc/loginService/LoginServiceApplicationTests.java
@@ -3,7 +3,10 @@ package com.edu.uptc.loginService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+    "RESEND_API_KEY=dummy",
+    "APP_ADMIN_EMAIL=test@example.com"
+})
 class LoginServiceApplicationTests {
 
 	@Test
