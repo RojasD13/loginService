98900b94acc523554da78cdcc8327f8953b90dc2
RojasD13 <94007095+RojasD13@users.noreply.github.com>
2025-10-13 15:26:57 -0500

    first commit

---
+++ src/test/java/com/edu/uptc/loginService/LoginServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.uptc.loginService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
