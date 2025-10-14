a1ba60ac1952d0a6378ca53c323329ca93124d75
RojasD13 <94007095+RojasD13@users.noreply.github.com>
2025-10-13 20:21:54 -0500

    feat: Adición de LoginRequest

---
+++ src/main/java/com/edu/uptc/loginService/dto/LoginRequest.java
@@ -0,0 +1,15 @@
+package com.edu.uptc.loginService.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class LoginRequest {
+    
+    private String email;
+    private String password;
+
+}
