f1cfe1569170c589756072cfc6c654eff22b2ea7
RojasD13 <94007095+RojasD13@users.noreply.github.com>
2025-10-13 21:32:04 -0500

    feat: se añade la clase AuthController refactorizada

--- src/main/java/com/edu/uptc/loginService/service/UserService.java
+++ src/main/java/com/edu/uptc/loginService/service/UserService.java
@@ -1,11 +1,16 @@
 package com.edu.uptc.loginService.service;
 
+import com.edu.uptc.loginService.dto.LoginRequest;
 import com.edu.uptc.loginService.model.User;
 import com.edu.uptc.loginService.repository.UserRepository;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
+import java.util.function.Supplier;
 
 @Service
 public class UserService {
@@ -20,8 +25,15 @@ public class UserService {
         return repo.findByEmail(email);
     }
 
+    public ResponseEntity<?> processLoginRequest(LoginRequest req) {
+        return processRequest(
+                () -> this.login(req.getEmail(), req.getPassword()),
+                HttpStatus.UNAUTHORIZED,
+                "Email o contraseña inválidos");
+    }
+
     @Transactional
-    public Optional<User> login(String email, String password) {
+    private Optional<User> login(String email, String password) {
         return repo.findByEmail(email)
                 .filter(user -> user.getPassword().equals(password))
                 .map(user -> {
@@ -30,14 +42,32 @@ public class UserService {
                 });
     }
 
+    public ResponseEntity<?> processLogout(LoginRequest req) {
+        return processRequest(
+                () -> this.logout(req.getEmail()),
+                HttpStatus.NOT_FOUND,
+                "Usuario no encontrado");
+    }
+
     @Transactional
-    public Optional<User> logout(String email) {
+    private Optional<User> logout(String email) {
         return repo.findByEmail(email)
                 .map(user -> {
                     user.setLogin(false);
                     repo.save(user);
                     return user;
                 });
+    }
+
+    private <T> ResponseEntity<?> processRequest(
+            Supplier<Optional<T>> action,
+            HttpStatus errorStatus,
+            String errorMessage) {
+        return action.get()
+                .<ResponseEntity<?>>map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity
+                        .status(errorStatus)
+                        .body(errorMessage));
     }
 
     public boolean isLoggedIn(String email) {
