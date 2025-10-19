f8285fb9045bfed47df2b9dc8e1f4f5955532aa9
RojasD13 <94007095+RojasD13@users.noreply.github.com>
2025-10-18 21:30:10 -0500

    fix: se crea dto para emailRequest y se modifica el authController para enpoint /isLogin

---
+++ src/main/java/com/edu/uptc/loginService/dto/EmailRequest.java
@@ -0,0 +1,12 @@
+package com.edu.uptc.loginService.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class EmailRequest {
+    private String email;
+}
